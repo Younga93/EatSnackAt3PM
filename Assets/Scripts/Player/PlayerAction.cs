edc29ee6cdf7513121ff14b758ee6d55c36134a7
Lee Hye Rim <37866532+lhr4426@users.noreply.github.com>
2025-07-29 21:04:46 +0900

    [feat] Player settings

---
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    private Dictionary<PlayerActionState, BaseAction> actionDict;
+    // TODO : Dictionary에 연결할 액션들 구현
+
+    Animator animator;
+
+    public void Init(PlayerController playerController)
+    {
+        this.playerController = playerController;
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        actionDict[playerController.state].PlayAction();
+    }
+}
