707ccc6c2de8a3b95943b527fc69a0ea2a227256
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-07-30 16:44:46 +0900

    [chore] 프리셋 배치 기즈모 출력 스크립트 & 스크립트 설명 추가

--- Assets/Scripts/Entities/Preset.cs
+++ Assets/Scripts/Entities/Preset.cs
@@ -9,6 +9,10 @@ public class Preset : MonoBehaviour
     public void Init(PresetSpawnManager spawnManager)
     { _spawnManager = spawnManager; }
 
+    /// <summary>
+    /// 해당 오브젝트의 자식 오브젝트들을 모두 오브젝트 풀로 되돌리는 함수
+    /// TODO: 이 오브젝트가 제거영역에 닿았을때 실행하도록 구현필요
+    /// </summary>
     public void ReturnEveryObjects()
     {
         ObstacleBase[] obstacles = GetComponentsInChildren<ObstacleBase>();
@@ -24,11 +28,7 @@ public class Preset : MonoBehaviour
             _spawnManager.ReturnItem(item);
         }
 
+        _spawnManager = null;
         Destroy(gameObject);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
     }
 }
--- Assets/Scripts/Managers/PresetSpawnManager.cs
+++ Assets/Scripts/Managers/PresetSpawnManager.cs
@@ -22,9 +22,9 @@ public enum ItemType
 
 public class PresetSpawnManager : MonoBehaviour
 {
-    private static PresetSpawnManager _instance;
-    public static PresetSpawnManager Instance { get { return _instance; } }
-
+    /// <summary>
+    /// 각 오브젝트마다 저장해놓을 오브젝트 풀
+    /// </summary>
     private ObjectPool<NormalObstacle> obstacleTopAPool;
     private ObjectPool<NormalObstacle> obstacleTopBPool;
     private ObjectPool<NormalObstacle> obstacleBotAPool;
@@ -39,6 +39,9 @@ public class PresetSpawnManager : MonoBehaviour
 
     private ObjectPool<SpeedUpItem> speedUpItemPool;
 
+    /// <summary>
+    /// 생성할 오브젝트 프리팹 목록
+    /// </summary>
     [Header("Prefabs")]
     [SerializeField] NormalObstacle obstacleTopAPrefab;
     [SerializeField] NormalObstacle obstacleTopBPrefab;
@@ -54,9 +57,15 @@ public class PresetSpawnManager : MonoBehaviour
 
     [SerializeField] SpeedUpItem SpeedUpItemPrefab;
 
+    /// <summary>
+    /// 사용할 프리셋 목록
+    /// </summary>
     [Header("Preset")]
     [SerializeField] GameObject[] presets;
 
+    /// <summary>
+    /// 오브젝트 풀을 초기화하는 함수
+    /// </summary>
     public void Init()
     {
         obstacleTopAPool = new ObjectPool<NormalObstacle>(obstacleTopAPrefab, 10, transform);
@@ -64,16 +73,21 @@ public class PresetSpawnManager : MonoBehaviour
         obstacleBotAPool = new ObjectPool<NormalObstacle>(obstacleBotAPrefab, 10, transform);
         obstacleBotBPool = new ObjectPool<NormalObstacle>(obstacleBotBPrefab, 10, transform);
 
-        //breakableObstaclePool = new ObjectPool<BreakableObstacle>(breakableObstaclePrefab, 10);
+        breakableObstaclePool = new ObjectPool<BreakableObstacle>(breakableObstaclePrefab, 10, transform);
 
-        //coinItemAPool = new ObjectPool<CoinItem>(coinItemAPrefab, 30);
-        //coinItemBPool = new ObjectPool<CoinItem>(coinItemBPrefab, 10);
+        coinItemAPool = new ObjectPool<CoinItem>(coinItemAPrefab, 30, transform);
+        coinItemBPool = new ObjectPool<CoinItem>(coinItemBPrefab, 10, transform);
 
-        //recoveryItemPool = new ObjectPool<RecoveryItem>(recoveryItemPrefab, 10);
+        recoveryItemPool = new ObjectPool<RecoveryItem>(recoveryItemPrefab, 10, transform);
 
-        //speedUpItemPool = new ObjectPool<SpeedUpItem>(SpeedUpItemPrefab, 10);
+        speedUpItemPool = new ObjectPool<SpeedUpItem>(SpeedUpItemPrefab, 10, transform);
     }
 
+    /// <summary>
+    /// 장애물 타입에 따라 장애물 풀에서 장애물을 가져오는 함수
+    /// </summary>
+    /// <param name="type">가져오고자 하는 장애물 타입</param>
+    /// <returns>실제로 가져온 장애물</returns>
     public GameObject GetObstacle(ObstacleType type)
     {
         switch (type)
@@ -87,6 +101,11 @@ public class PresetSpawnManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 아이템 타입에 따라 아이템 풀에서 아이템을 가져오는 함수
+    /// </summary>
+    /// <param name="type">가져오고자 하는 아이템 타입</param>
+    /// <returns>실제로 가져온 아이템</returns>
     public GameObject GetItem(ItemType type)
     {
         switch (type)
@@ -99,13 +118,23 @@ public class PresetSpawnManager : MonoBehaviour
         }
     }
 
-    public void MakePreset(GameObject preset, Transform parent)
+    /// <summary>
+    /// 프리셋 정보를 바탕으로 오브젝트 풀에서 가져와 실제로 배치하는 함수
+    /// 되게 쓸때없이 반복적이고 긴데 개선할 방법이 있는지 찾아봐야 할 듯
+    /// </summary>
+    /// <param name="preset">사용하고자 하는 배치 프리셋 정보</param>
+    /// <param name="parent">가져온 오브젝트들을 묶어둘 부모 오브젝트</param>
+    public void PlaceObjects(GameObject preset, Transform parent)
     {
         Transform[] obstacleTopAPos = preset.transform.Find("ObstacleTopASpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
         Transform[] obstacleTopBPos = preset.transform.Find("ObstacleTopBSpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
         Transform[] obstacleBotAPos = preset.transform.Find("ObstacleBotASpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
         Transform[] obstacleBotBPos = preset.transform.Find("ObstacleBotBSpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
         Transform[] obstacleBreakPos = preset.transform.Find("ObstacleBreakSpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
+        Transform[] coinItemAPos = preset.transform.Find("CoinItemASpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
+        Transform[] coinItemBPos = preset.transform.Find("CoinItemBSpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
+        Transform[] recoveryItemPos = preset.transform.Find("RecoverItemSpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
+        Transform[] speedUpItemPos = preset.transform.Find("SpeedUpItemSpawnPoints").GetComponentsInChildren<Transform>().Where(t => t.position != Vector3.zero).ToArray();
 
         foreach (Transform t in obstacleTopAPos)
         {
@@ -137,8 +166,37 @@ public class PresetSpawnManager : MonoBehaviour
             go.transform.parent = parent;
             go.transform.localPosition = t.position;
         }
+        foreach (Transform t in coinItemAPos)
+        {
+            GameObject go = GetItem(ItemType.CoinA);
+            go.transform.parent = parent;
+            go.transform.localPosition = t.position;
+        }
+        foreach (Transform t in coinItemBPos)
+        {
+            GameObject go = GetItem(ItemType.CoinB);
+            go.transform.parent = parent;
+            go.transform.localPosition = t.position;
+        }
+        foreach (Transform t in recoveryItemPos)
+        {
+            GameObject go = GetItem(ItemType.Recovery);
+            go.transform.parent = parent;
+            go.transform.localPosition = t.position;
+        }
+        foreach (Transform t in speedUpItemPos)
+        {
+            GameObject go = GetItem(ItemType.SpeedUp);
+            go.transform.parent = parent;
+            go.transform.localPosition = t.position;
+        }
+
     }
 
+    /// <summary>
+    /// Obstacle타입의 오브젝트를 오브젝트 풀로 되돌리는 함수
+    /// </summary>
+    /// <param name="obj">되돌리고자 하는 장애물 오브젝트</param>
     public void ReturnObstacle(ObstacleBase obj)
     {
         ObstacleType type = obj.GetObstacleType();
@@ -153,6 +211,10 @@ public class PresetSpawnManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Item타입의 오브젝트를 오브젝트 풀로 되돌리는 함수
+    /// </summary>
+    /// <param name="item">되돌리고자 하는 아이템 오브젝트</param>
     public void ReturnItem(ItemBase item)
     {
         ItemType type = item.GetItemType();
@@ -166,25 +228,26 @@ public class PresetSpawnManager : MonoBehaviour
         }
     }
 
-    private void Start()
+    /// <summary>
+    /// 랜덤한 배치 프리셋을 선택하고 이를 지정 위치에 생성하는 함수
+    /// </summary>
+    /// <param name="spawnPos">해당 배치 프리셋을 생성할 위치</param>
+    public void MakePreset(Vector3 spawnPos)
     {
-        Init();
-
-        if (presets[0] != null)
+        int n = Random.Range(0, presets.Length); // 랜덤한 프리셋을 선택
+        if (n < presets.Length) // 해당 프리셋이 실제로 존재하는 지 확인
         {
-            GameObject go = new GameObject("empty");
+            GameObject go = new GameObject("[PRESET]");
+            go.transform.position = spawnPos;
             go.AddComponent<Preset>();
             go.GetComponent<Preset>().Init(this);
-            MakePreset(presets[0], go.transform);
-            StartCoroutine(Return(go.GetComponent<Preset>()));
+            PlaceObjects(presets[n], go.transform);
         }
-
     }
 
-    IEnumerator Return(Preset go)
+    private void Awake()
     {
-        yield return new WaitForSeconds(1f);
-        go.ReturnEveryObjects();
+        Init();
     }
 
 }
