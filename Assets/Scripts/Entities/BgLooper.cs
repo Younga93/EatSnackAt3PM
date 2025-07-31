d449e0f912ddb961d141f0de96005ca0a72b0694
pcw30215 <scout386@naver.com>
2025-07-31 12:27:15 +0900

    [chore] 배경 바닥 수정 및 반복 생성 로직 작성
    새로운 이미지 생성으로 배경과 바닥 관련 사항 수정
    이후, 배경 오브젝트 프리팹 생성 작업

---
+++ Assets/Scripts/Entities/BgLooper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BgLooper : MonoBehaviour
+{
+    public int obstacleCount = 0;
+    public Vector3 obstacleLastPosition = Vector3.zero;
+
+    void Start()
+    {
+        ObstacleBase[] obstacles = GameObject.FindObjectsOfType<ObstacleBase>();
+        obstacleLastPosition = obstacles[0].transform.position;
+        obstacleCount = obstacles.Length;        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered: " + collision.name);
+
+        ObstacleBase obstacle = collision.GetComponent<ObstacleBase>();        
+    }
+}
