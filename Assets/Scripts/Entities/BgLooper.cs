a5f740f108fc077c2949958bbd701f91e9c64d69
pcw30215 <scout386@naver.com>
2025-07-31 12:42:30 +0900

    [chore] BgLooper 코드 수정
    저장 불가능 부분 주석 처리 및 변수 임의 지정 (Obstacle → ObstacleBase)

--- Assets/Scripts/Entities/BgLooper.cs
+++ Assets/Scripts/Entities/BgLooper.cs
@@ -4,20 +4,41 @@ using System.Collections.Generic;
 
 public class BgLooper : MonoBehaviour
 {
+    public int numBgCount = 4;  
+
     public int obstacleCount = 0;
     public Vector3 obstacleLastPosition = Vector3.zero;
 
     void Start()
     {
-        ObstacleBase[] obstacles = GameObject.FindObjectsOfType<ObstacleBase>();
+        ObstacleBase[] obstacles = GameObject.FindObjectsOfType<ObstacleBase>();  // 씬에 존재하는 모든 ObstacleBase 객체를 배열로 가져오기 (임시 코드 수정 상태)
         obstacleLastPosition = obstacles[0].transform.position;
-        obstacleCount = obstacles.Length;        
+        obstacleCount = obstacles.Length;
+
+        //for (int i = 0; i < obstacleCount; i++)
+        //{
+        //    obstacleLastPosition = obstacles[i].SetRandomPlace(obstacleLastPosition, obstacleCount);
+        //}
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Triggered: " + collision.name);
 
-        ObstacleBase obstacle = collision.GetComponent<ObstacleBase>();        
+        if (collision.CompareTag("BackGround"))
+        {
+            float widthOfBgObject = ((BoxCollider2D)collision).size.x;
+            Vector3 pos = collision.transform.position;
+
+            pos.x += widthOfBgObject * numBgCount;
+            collision.transform.position = pos;
+            return;
+        }
+
+        ObstacleBase obstacle = collision.GetComponent<ObstacleBase>(); // 충돌한 객체가 ObstacleBase인지 확인 (임시 코드 수정 상태)
+        //if (obstacle)
+        //{
+        //    obstacleLastPosition = obstacle.SetRandomPlace(obstacleLastPosition, obstacleCount);
+        //}
     }
 }
