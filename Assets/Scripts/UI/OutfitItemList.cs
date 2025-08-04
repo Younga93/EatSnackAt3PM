68af0a3344588198aac4d6a03044ccb7d27c033e
Lee Hye Rim <37866532+lhr4426@users.noreply.github.com>
2025-08-04 11:30:06 +0900

    [feat] 상점 ScrollView로 변경

--- Assets/Scripts/UI/OutfitItemList.cs
+++ Assets/Scripts/UI/OutfitItemList.cs
@@ -6,25 +6,15 @@ using UnityEngine;
 public class OutfitItemList : MonoBehaviour
 {
     [SerializeField] GameObject itemPanel;
+    [SerializeField] Transform content;
 
     // Start is called before the first frame update
     void Start()
     {
-        float initialX = 360f;
-        float initialY = 565f;
-        int index = 0;
         foreach (OutfitItemBase item in OutfitItemData.allOutfitItems) //for문으로 할걸.
         {
-            GameObject go = Instantiate(itemPanel, this.transform);
-
-            float x = initialX + (400 * index++);
-            float y = initialY;
-            
-            go.GetComponent<RectTransform>().position = new Vector2(x,y);
-            //go.transform.position = new Vector2(x, 0);
+            GameObject go = Instantiate(itemPanel, content);
             go.GetComponent<OutfitItemPanelUI>().Setting(item.Id);
         }
-
-        //To do: 아이템 4개 이상일 경우, 스크롤하는 기능 추가
     }
 }
